60b7ff2c3faf1e2799806d5ddc143d0866ee44d5
Kwon770 <sckwon770@gmail.com>
2020-03-03 08:35:15 +0900

    1-1-1 Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
+import { createStore } from "redux";
+
 const plus = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+// reducer : countModifier : the only function to change data
+// Store : countStore : where the data is saved
+const countModifier = (count = 0) => {
+  return count;
+};
+const countStore = createStore(countModifier);
+
 let count = 0;
 
-number.innerText = count;
+// number.innerText = count;
 
-const updateText = () => {
-  number.innerText = count;
-};
+// const updateText = () => {
+//   number.innerText = count;
+// };
 
-const handleAdd = () => {
-  count++;
-  updateText();
-};
+// const handleAdd = () => {
+//   count++;
+//   updateText();
+// };
 
-const handleMinus = () => {
-  count--;
-  updateText();
-};
+// const handleMinus = () => {
+//   count--;
+//   updateText();
+// };
 
-plus.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+// plus.addEventListener("click", handleAdd);
+// minus.addEventListener("click", handleMinus);
