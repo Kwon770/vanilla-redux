30223d207efc1c11dfcf1a2d001cb33bd9bd90b3
Kwon770 <sckwon770@gmail.com>
2020-03-04 13:53:06 +0900

    2-1 State Mutation;

--- src/index.js
+++ src/index.js
@@ -7,10 +7,13 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+// https://redux.js.org/introduction/three-principles/
+// Third of three principles : You must not mutate(mofify) state.
+// Return new state object, instead of mutating thre previous state.
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [];
+      return [...state, { text: action.text, id: Date.now() }];
     case DELETE_TODO:
       return [];
     default:
@@ -19,6 +22,8 @@ const reducer = (state = [], action) => {
 };
 
 const store = createStore(reducer);
+
+store.subscribe();
 
 const onSubmit = e => {
   e.preventDefault();
