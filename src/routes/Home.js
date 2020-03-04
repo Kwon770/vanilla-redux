af8cdcf12096eb049d97665ab3f0f99ab25d935b
Kwon770 <sckwon770@gmail.com>
2020-03-05 08:21:23 +0900

    4-3 mapStateToProps

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function Home() {
+function Home({ toDos }) {
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
@@ -16,9 +16,15 @@ function Home() {
         <input type="text" value={text} onChange={onChange}></input>
         <button>Add</button>
       </form>
-      <ul></ul>
+      <ul>{JSON.stringify(toDos)}</ul>
     </>
   );
 }
 
-export default Home;
+// Send state of Store to connected component as props
+function mapStateToProps(state, ownProps) {
+  return { toDos: state };
+}
+
+// connect() : The function connects react component to redux store
+export default connect(mapStateToProps)(Home);
